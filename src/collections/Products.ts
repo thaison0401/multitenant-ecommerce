4ddb37ce4fd405c1fa4336edc41609703f920f7f
thaisonlun <thaisonlun553@gmail.com>
2025-11-08 16:29:39 +0700

    13: api filters and sorting

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -28,6 +28,13 @@ export const Products: CollectionConfig = {
     },
 
     {
+      name: "tags",
+      type: "relationship",
+      relationTo: "tags",
+      hasMany: true,
+    },
+
+    {
       name: "image",
       type: "upload",
       relationTo: "media",
--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useProductFilters } from "../../hooks/use-product-filters";
 
 interface Props {
   category?: string;
 }
 
 export const ProductList = ({ category }: Props) => {
+  const [filters] = useProductFilters();
+
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
-    trpc.products.getMany.queryOptions({ category })
+    trpc.products.getMany.queryOptions({ category, ...filters })
   );
 
   return (
-    <div className="grid grid-cols-1 sm:gird-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-clos-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
       {data?.docs.map((product) => (
         <div key={product.id} className="border rounded-md bg-white p-4">
           <h2 className="text-xl font-medium">{product.name}</h2>
