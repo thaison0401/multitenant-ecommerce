633cb2e1dc6502be8f95e746ecab43c2229d8e4e
thaisonlun <thaisonlun553@gmail.com>
2025-11-12 11:26:10 +0700

    16: tenant pages

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/page.tsx
@@ -4,32 +4,30 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { DEFAULT_LIMIT } from "@/constants";
 import { getQueryClient, trpc } from "@/trpc/server";
 
-import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { loadProductFilters } from "@/modules/products/search-params";
 
 interface Props {
-  params: Promise<{
-    category: string;
-  }>;
   searchParams: Promise<SearchParams>;
+  params: Promise<{ slug: string }>;
 }
 
 const Page = async ({ params, searchParams }: Props) => {
-  const { category } = await params;
+  const { slug } = await params;
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
   void queryClient.prefetchInfiniteQuery(
     trpc.products.getMany.infiniteQueryOptions({
       ...filters,
-      category,
+      tenantSlug: slug,
       limit: DEFAULT_LIMIT,
     })
   );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProductListView category={category} />
+      <ProductListView tenantSlug={slug} narrowView />
     </HydrationBoundary>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,12 +2,14 @@ import { createTRPCRouter } from "../init";
 
 import { authRouter } from "@/modules/auth/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
+import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
+  tenants: tenantsRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });
