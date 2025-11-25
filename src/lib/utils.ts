f41ad079575c514aca876aaf91f221caa20b6119
thaisonlun <thaisonlun553@gmail.com>
2025-11-25 15:14:23 +0700

    28: deployment fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(tenantSlug: string) {
-  // In development mode, use normal routing
-  if (process.env.NODE_ENV === "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!
+  );
+  // In development or subdomain routing disabled mode, use normal routing
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
